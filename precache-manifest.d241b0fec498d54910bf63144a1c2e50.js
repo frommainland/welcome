11461235236c6808fa387b410c391f7f3098dd4c
frommainland <caihehuang@gmail.com>
2020-06-10 22:53:47 +0800

    Updates

---
+++ precache-manifest.d241b0fec498d54910bf63144a1c2e50.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10766211c5494971acdcbd1f15a1b81",
+    "url": "/welcome/index.html"
+  },
+  {
+    "revision": "ddf939d7d58b9da0aaa0",
+    "url": "/welcome/static/css/main.47689c79.chunk.css"
+  },
+  {
+    "revision": "647a17ceb9e1515ebcc9",
+    "url": "/welcome/static/js/2.d2bf1f59.chunk.js"
+  },
+  {
+    "revision": "c0fa90f3c9667fc5b8411012aeed21e0",
+    "url": "/welcome/static/js/2.d2bf1f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf939d7d58b9da0aaa0",
+    "url": "/welcome/static/js/main.3f0fcf6c.chunk.js"
+  },
+  {
+    "revision": "eeac56d4507781578bf0",
+    "url": "/welcome/static/js/runtime-main.3b6f042e.js"
+  },
+  {
+    "revision": "7e3a7ab73d528f5769c551995c983671",
+    "url": "/welcome/static/media/earth.7e3a7ab7.svg"
+  },
+  {
+    "revision": "431ceedf7a1cc748051140e4064150f5",
+    "url": "/welcome/static/media/earth1.431ceedf.svg"
+  },
+  {
+    "revision": "36bca3d1e2a5956a0fa29fff4046b804",
+    "url": "/welcome/static/media/earth2.36bca3d1.svg"
+  },
+  {
+    "revision": "0504f0b314e29c7cd93456430ddf2f51",
+    "url": "/welcome/static/media/fire.0504f0b3.svg"
+  },
+  {
+    "revision": "8baacde0eab17f3c15b1b58280b0ddf0",
+    "url": "/welcome/static/media/fire.8baacde0.svg"
+  },
+  {
+    "revision": "5093a337ec0febc14d5070b711fd5aa4",
+    "url": "/welcome/static/media/innovationStudio-logo-white.5093a337.svg"
+  },
+  {
+    "revision": "ef7cd7c677fd8ed1472379faf888fcdf",
+    "url": "/welcome/static/media/innovationStudio-logo.ef7cd7c6.svg"
+  },
+  {
+    "revision": "47a040077412ec68e06c3d979cb89f5d",
+    "url": "/welcome/static/media/metal-half.47a04007.svg"
+  },
+  {
+    "revision": "6dcd2aa34bafbc1dbdaa047f8db53f44",
+    "url": "/welcome/static/media/metal.6dcd2aa3.svg"
+  },
+  {
+    "revision": "01ba79d0992ab1594e1504af6b15e279",
+    "url": "/welcome/static/media/water.01ba79d0.svg"
+  },
+  {
+    "revision": "818f11360d0fa65a85aa03ade4ea2d21",
+    "url": "/welcome/static/media/water.818f1136.svg"
+  },
+  {
+    "revision": "f5667d8901fc59e1d3923e4f5494a947",
+    "url": "/welcome/static/media/wood.f5667d89.svg"
+  }
+]);
